49cf830f0f517740b16e4279939b1db44bae83fa
omenkish <omenkish@gmail.com>
2020-01-14 15:23:39 +0100

    viewtool initial commit

---
+++ lib/omenkish_view_tool.rb
@@ -0,0 +1,7 @@
+require "omenkish_view_tool/version"
+require "omenkish_view_tool/renderer"
+
+module OmenkishViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/omenkish_view_tool/version.rb
@@ -0,0 +1,3 @@
+module OmenkishViewTool
+  VERSION = "0.1.0"
+end
